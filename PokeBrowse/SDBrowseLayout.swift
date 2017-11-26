42dce80c0d3970d263a2080702539416d4f05177
Scott Daniel <sdaniel@adobe.com>
2017-11-25 22:48:31 -0800

    circular layout working, but buggy

--- PokeBrowse/SDBrowseLayout.swift
+++ PokeBrowse/SDBrowseLayout.swift
@@ -12,21 +12,35 @@ class SDBrowseLayout: UICollectionViewLayout
 {
     private let itemWidth: CGFloat = 50
     private let itemHeight: CGFloat = 50
-    private let spacing: CGFloat = 10
+    private var layoutAttributes = [UICollectionViewLayoutAttributes]()
     
-    private func itemOffset(_ index: Int) -> CGFloat
-    {
-        let x = CGFloat(index) * self.itemWidth + self.spacing * max(CGFloat(index), 0.0)
-        print("\(index): \(x)")
-        if (x < 0) {
-            print("break")
+    private var radius: CGFloat {
+        get {
+            guard let collectionView = self.collectionView else { return CGFloat(0) }
+            return min(collectionView.frame.width, collectionView.frame.height) / 2.0
         }
-        return x
     }
     
     override func prepare()
     {
-        print("prepare")
+        guard let collectionView = self.collectionView else {
+            return
+        }
+        
+        let numberOfItems = collectionView.numberOfItems(inSection: 0)
+        let r = min(collectionView.frame.width, collectionView.frame.height) / 2.0
+        
+        self.layoutAttributes = (0..<numberOfItems).map {
+            [itemWidth = self.itemWidth, itemHeight = self.itemHeight] index in
+            
+            let attribute = UICollectionViewLayoutAttributes(forCellWith: IndexPath(item: index, section: 0))
+            let theta = 2 * CGFloat.pi * CGFloat(index) / CGFloat(numberOfItems) + 2 * CGFloat.pi *  collectionView.contentOffset.x / collectionView.contentSize.width
+            
+            
+            attribute.frame = CGRect(x: collectionView.frame.midX + collectionView.contentOffset.x + r * cos(theta), y: collectionView.frame.midY + r * sin(theta), width: itemWidth, height: itemHeight)
+            
+            return attribute
+        }
     }
     
     override var collectionViewContentSize: CGSize {
@@ -35,40 +49,19 @@ class SDBrowseLayout: UICollectionViewLayout
                 return CGSize.zero
             }
             
-            let count = collectionView.numberOfItems(inSection: 0)
-            let width = self.itemOffset(count) + collectionView.frame.midX
+            let width = 2 * CGFloat.pi * self.radius
             return CGSize(width: width, height: collectionView.bounds.height)
         }
     }
     
     override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes?
     {
-        guard let collectionView = self.collectionView else {
-            return nil
-        }
-        
-        let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath)
-        
-        attributes.frame = CGRect(x: collectionView.frame.midX + self.itemOffset(indexPath.item), y: collectionView.frame.midY, width: self.itemWidth, height: self.itemHeight)
-        
-        return attributes
+        return layoutAttributes[indexPath.item]
     }
     
     override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]?
     {
-        guard let numberOfItems = self.collectionView?.numberOfItems(inSection: 0) else {
-            return nil
-        }
-        
-        let minIndex = Int(floor(max(rect.minX, 0.0) / (self.itemWidth + self.spacing)))
-        let count = Int(floor(rect.width / (self.itemWidth + self.spacing)))
-        let maxIndex = min(minIndex + count, numberOfItems - 1)
-        
-        let attributes = (minIndex...maxIndex).map {
-            self.layoutAttributesForItem(at: IndexPath(item: $0, section: 0))!
-        }
-        
-        return attributes
+        return self.layoutAttributes
     }
     
     override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool
--- PokeBrowse/ViewController.swift
+++ PokeBrowse/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+let NUM_ITEMS = 10
+
 class ViewController: UICollectionViewController {
 
     override func viewDidLoad() {
@@ -18,14 +20,14 @@ class ViewController: UICollectionViewController {
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1000
+        return NUM_ITEMS
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
     {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
         
-        let hue = CGFloat(indexPath.item) / 1000.0
+        let hue = CGFloat(indexPath.item) / CGFloat(NUM_ITEMS)
         cell.backgroundColor = UIColor(hue: hue, saturation: 1.0, brightness: 1.0, alpha: 1.0)
         
         return cell
